74cd29e7d3271af023c78a9fd67d3f4fec7d48d2
thefireflyer <theflyingfire@users.noreply.github.com>
2023-06-28 17:15:29 -0700

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import fs from 'fs'
+// import basicSsl from '@vitejs/plugin-basic-ssl'
+import mkcert from 'vite-plugin-mkcert'
+
+
+const certificate = fs.readFileSync('cert.pem')
+const key = fs.readFileSync('key.pem')
+const https = () => ({ cert: certificate, key: key })
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	server: {
+		https: false
+	},
+	preview: {
+		https: true
+	},
+	plugins: [sveltekit()],
 };
 
 export default config;
