3abc14c550ef4126939a2f02488953b687ef661c
thefireflyer <theflyingfire@users.noreply.github.com>
2024-07-23 20:59:02 -0700

    minor

--- src/lib/old/supabase.ts
+++
@@ -1,9 +0,0 @@
-import { createClient, type User } from '@supabase/supabase-js'
-import { PUBLIC_PROJECT_URL, PUBLIC_ANON_API_KEY } from "$env/static/public";
-import { writable, type Writable } from 'svelte/store';
-
-export const supabase = createClient(PUBLIC_PROJECT_URL, PUBLIC_ANON_API_KEY)
-
-export const user: Writable<User | null | "loading"> = writable(null)//"loading")
-
-// supabase.auth.getUser().then(response => user.set(response.data.user))
--- src/lib/old/userFirebase.ts
+++
@@ -1,4 +0,0 @@
-import { writable } from "svelte/store";
-import { auth } from "./firebase";
-
-export const user = writable(auth.currentUser)
