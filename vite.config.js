be1c4670503e1965d6fc9b5e1a9c978b49566150
thefireflyer <the.fireflyer@protonmail.com>
2025-01-25 12:29:58 -0800

    0

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import fs from 'fs'
-import basicSsl from '@vitejs/plugin-basic-ssl'
-import mkcert from 'vite-plugin-mkcert'
-
-
-// const certificate = fs.readFileSync('cert.pem')
-// const key = fs.readFileSync('key.pem')
-// const https = () => ({ cert: certificate, key: key })
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	server: {
-		https: false
-	},
-	preview: {
-		// https: true
-	},
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit()
+	],
 };
 
 export default config;
